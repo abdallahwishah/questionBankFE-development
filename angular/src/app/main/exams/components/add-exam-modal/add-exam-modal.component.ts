bc2e38d092a5c3bb0efc5e3f043f61cefab58cfb
abdallahwishah <84712712+abdallahwishah@users.noreply.github.com>
2025-01-13 21:09:23 +0200

    s

--- angular/src/app/main/exams/components/add-exam-modal/add-exam-modal.component.ts
+++ angular/src/app/main/exams/components/add-exam-modal/add-exam-modal.component.ts
@@ -1,55 +1,47 @@
 import { Component, Injector, OnInit } from '@angular/core';
 import { DialogSharedModule } from '@app/shared/components/dialog-shared/dialog-shared.module';
 import { DialogSharedService } from '@app/shared/components/dialog-shared/dialog-shared.service';
-import { AutoCompleteComponent } from '@app/shared/components/fields/auto-complete/auto-complete.component';
-import { DateComponent } from '@app/shared/components/fields/date/date.component';
 import { DropdownFieldComponent } from '@app/shared/components/fields/dropdown-field/dropdown-field.component';
 import { UniqueNameComponents } from '@app/shared/Models/UniqueNameComponents';
 import { AppComponentBase } from '@shared/common/app-component-base';
 import { ExamTemplatesServiceProxy } from '@shared/service-proxies/service-proxies';
-import { SkeletonComponent } from "../../../../shared/components/skeleton/skeleton.component";
+import { SkeletonComponent } from '../../../../shared/components/skeleton/skeleton.component';
 
 @Component({
-  selector: 'app-add-exam-modal',
-  standalone: true,
-  imports: [DropdownFieldComponent, DialogSharedModule, DateComponent, AutoCompleteComponent, SkeletonComponent],
-  templateUrl: './add-exam-modal.component.html',
-  styleUrl: './add-exam-modal.component.css'
+    selector: 'app-add-exam-modal',
+    standalone: true,
+    imports: [DropdownFieldComponent, DialogSharedModule, SkeletonComponent],
+    templateUrl: './add-exam-modal.component.html',
+    styleUrl: './add-exam-modal.component.css',
 })
-export class AddExamModalComponent  extends AppComponentBase implements OnInit {
+export class AddExamModalComponent extends AppComponentBase implements OnInit {
     Add_Test_dialog = UniqueNameComponents.Add_Test_dialog;
     saving = false;
-    examTemplates:any;
+    examTemplates: any;
     constructor(
         injector: Injector,
         private _DialogSharedService: DialogSharedService,
         private _examTemplatesServiceProxy: ExamTemplatesServiceProxy,
-
     ) {
         super(injector);
     }
     ngOnInit(): void {
         this._examTemplatesServiceProxy
-        .getAll(
-            undefined,
-            undefined,
-            undefined,
-            undefined,
-            undefined,
-            undefined,
-            undefined,
-            undefined,
-        ).subscribe((result:any) => {
-            this.examTemplates = result.items.map((item) => ({
-                id: item.examTemplate.id,
-                name: item.examTemplate.name,
-            }));
-
-        });
-
+            .getAll(undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined)
+            .subscribe((result: any) => {
+                this.examTemplates = result.items.map((item) => ({
+                    id: item.examTemplate.id,
+                    name: item.examTemplate.name,
+                }));
+            });
     }
 
-    Save() {}
+    Save() {
+        this._examTemplatesServiceProxy.generateExamByTemplate(this.examTemplates).subscribe(() => {
+            this.notify.info(this.l('SavedSuccessfully'));
+            this.closeDialog();
+        });
+    }
 
     closeDialog() {
         this._DialogSharedService.hideDialog(this.Add_Test_dialog);
