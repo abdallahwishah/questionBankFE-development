b451a82707b01c0dab367c2b37f2b5eb7eef8c0a
abdallahwishah <84712712+abdallahwishah@users.noreply.github.com>
2025-03-24 12:39:14 +0200

    feat: extend StudentComponent to integrate SignalR for chat functionality

--- angular/src/app/student/student.component.ts
+++ angular/src/app/student/student.component.ts
@@ -1,18 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector, OnInit } from '@angular/core';
+import { ChatSignalrService } from '@app/shared/layout/chat/chat-signalr.service';
 import { NavigationEnd, Router } from '@node_modules/@angular/router';
+import { AppComponentBase } from '@shared/common/app-component-base';
+import { SignalRHelper } from 'shared/helpers/SignalRHelper';
 
 @Component({
     selector: 'app-student',
     templateUrl: './student.component.html',
     styleUrls: ['./student.component.css'],
 })
-export class StudentComponent implements OnInit {
-    constructor(private _router: Router) {}
-    ngOnInit() {
-   
-   
-     
+export class StudentComponent extends AppComponentBase {
+    constructor(
+        injector: Injector,
+        private _router: Router,
+        private _chatSignalrService: ChatSignalrService,
+    ) {
+        super(injector);
     }
-
-    
+    ngOnInit() {
+        if (this.appSession.application) {
+            SignalRHelper.initSignalR(() => {
+                this._chatSignalrService.init();
+            });
+        }
+    }
 }
